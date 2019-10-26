bef446197318801ef36add229c59a12431d9796c
Johanes <antoniusjohanes99@gmail.com>
2019-10-26 15:19:45 +0700

    add access to database

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
 const express = require("express");
+const mysql = require('mysql');
+const moment = require('moment');
 
 const app = express();
+const defaultStatus = "pending";
+
+
+function addTransaction(idUser, virtualAccount, idMovie, idSchedule, seat, response) {
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'ws-transaksi'
+    });
+
+    var now = moment().format("YYYY-MM-DD HH:mm:ss");
+    var idTransaksi = 0;
+
+    connection.connect();
+
+    var query = `INSERT INTO TransaksiTiket \
+            (idUser, virtualAccount, idMovie, idSchedule, seat, creationTime, status) \
+            VALUE (${idUser},${virtualAccount},${idMovie},${idSchedule},${seat},'${now}','${defaultStatus}')`;
+
+    connection.query(query, function (err, result, fields) {
+        if (err) response.sendStatus(400).send("Wrong Query!");
+        idTransaksi = result.insertId;
+
+        response.send({ idTransaksi: idTransaksi });
+    });
+
+    connection.end();
+
+}
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 
 app.get('/transaksi', function (request, response) {
-    response.send("Hello world!");
+    let idUser = request.query.idUser;
+    let virtualAccount = request.query.virtualAccount;
+    let idMovie = request.query.idMovie;
+    let idSchedule = request.query.idSchedule;
+    let seat = request.query.seat;
+    addTransaction(idUser, virtualAccount, idMovie, idSchedule, seat, response);
 });
 
 app.listen(3000, function () {
