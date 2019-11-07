ac96ea5fb2b1064a15d452cb71cada7a0bd9d0f1
louis <louiscahyadi23@gmail.com>
2019-11-07 09:46:06 +0700

    edit transaction done

--- index.js
+++ index.js
@@ -33,6 +33,63 @@ function addTransaction(idUser, virtualAccount, idMovie, idSchedule, seat, respo
     connection.end();
 }
 
+function editCreationTime(idTransaksi, newStatus, response) {
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'ws-transaksi'
+    });
+
+    connection.connect();
+
+    var query = `UPDATE TransaksiTiket SET status = ${newStatus} where idTransaksi = ${idTransaksi}`;
+    connection.query(query, function (err, result, fields) {
+        if (err) response.sendStatus(400).send("Wrong Query!");
+
+        response.send({ 
+            idTransaksi: idTransaksi,
+            status: newStatus
+        });
+    });
+
+    connection.end();
+}
+
+function editTransaction(idTransaksi, response) {
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'ws-transaksi'
+    });
+
+    var now = moment();
+    var waktuTransaksiDibuat;
+    var selisihWaktuBookingBayar;
+
+    connection.connect();
+    var query = `SELECT creationTime from TransaksiTiket where idTransaksi = ${idTransaksi}`;
+    connection.query(query, async function (err, result, fields) {
+        if (err) response.sendStatus(400).send("Wrong Query!");
+        await (waktuTransaksiDibuat = result[0].creationTime);
+        waktuTransaksiDibuat = moment(waktuTransaksiDibuat);
+        selisihWaktuBookingBayar = moment.duration(now.diff(waktuTransaksiDibuat, 'seconds'))
+        // console.log(selisihWaktuBookingBayar);
+
+        if (selisihWaktuBookingBayar > 120){
+            var statusTerkini = "\'cancelled\'";
+        } else {
+            var statusTerkini = "\'success\'";
+        }    
+
+        editCreationTime(idTransaksi, statusTerkini, response);
+    });
+
+    connection.end();
+}
+
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
@@ -46,6 +103,12 @@ app.post('/transaksi', function (request, response) {
     let idSchedule = request.body.idSchedule;
     let seat = request.body.seat;
     addTransaction(idUser, virtualAccount, idMovie, idSchedule, seat, response);
+});
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.post('/edit', function(request, response) {
+    let idTransaksi = request.body.idTransaksi;
+    editTransaction(idTransaksi, response);
 });
 
 app.listen(3000, function () {
