ccfb30ac10636bfb2b0298231d1fd3ab7f9b4045
Johanes <antoniusjohanes99@gmail.com>
2019-11-20 15:39:14 +0700

    cope .env to aws

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ chmod 400 $AWS_VM_PEM
 ssh -o "StrictHostKeyChecking=no" -i $AWS_VM_PEM $AWS_VM_user@$AWS_VM_ip "rm -rf ws-transaksi"
 ssh -o "StrictHostKeyChecking=no" -i $AWS_VM_PEM $AWS_VM_user@$AWS_VM_ip "mkdir ws-transaksi"
 scp -o "StrictHostKeyChecking=no" -i $AWS_VM_PEM * $AWS_VM_user@$AWS_VM_ip:~/ws-transaksi
+scp -o "StrictHostKeyChecking=no" -i $AWS_VM_PEM .env $AWS_VM_user@$AWS_VM_ip:~/ws-transaksi
 ssh -o "StrictHostKeyChecking=no" -i $AWS_VM_PEM $AWS_VM_user@$AWS_VM_ip "bash" < ./run.sh
